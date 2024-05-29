7464ccfde6573bb7a74bf91dfe47438f72f35bf4
nochoice555 <nochoice555@gmail.com>
2024-05-29 21:55:10 +0300

    add loop

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,15 @@
 use std::io::{self, Write};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    // println!("Logs from your program will appear here!");
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
 
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    let command = input.trim();
-    println!("{}: command not found", command);
+        let command = input.trim();
+        println!("{}: command not found", command);
+    }
 }
