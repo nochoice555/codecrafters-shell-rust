4190d54403c857072f8c186a1d28e48c6a052cc5
nochoice555 <nochoice555@gmail.com>
2024-05-29 21:48:06 +0300

    not found command

--- src/main.rs
+++ src/main.rs
@@ -13,4 +13,6 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+    let command = input.trim();
+    print!("{}: command not found", command);
 }
