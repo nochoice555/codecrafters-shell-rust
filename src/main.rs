d21452aff6c37a79d20c7998db94c8d56213b835
nochoice555 <nochoice555@gmail.com>
2024-05-30 22:17:53 +0300

    exit buildin

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,50 @@
-#[allow(unused_imports)]
-use std::io::{self, Write};
+use std::{io::Error, str::FromStr};
+use std::{
+    io::{self, Write},
+    process::exit,
+};
+
+enum Commands {
+    Exit,
+    NotFound,
+}
+
+impl FromStr for Commands {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "exit" => Ok(Self::Exit),
+            _ => Ok(Self::NotFound),
+        }
+    }
+}
 
 fn main() {
-    loop {
-        print!("$ ");
-        io::stdout().flush().unwrap();
-        
-        let stdin = io::stdin();
-        let mut input = String::new();
-        stdin.read_line(&mut input).unwrap();
+    print!("$ ");
 
-        let command = input.trim();
-        println!("{}: command not found", command);
+    loop {
+        let mut input = String::new();
+        read_input_to_buff(&mut input);
+
+        let [command, code] = parse_command(&mut input)[..] else {return;};
+        
+        match command.parse() {
+            Ok(cmd) => match cmd {
+                Commands::Exit => exit(code.parse().unwrap_or(0)),
+                Commands::NotFound => println!("{}: command not found", input),
+            },
+            Err(e) => println!("{}", e),
+        }
     }
+}
+
+fn read_input_to_buff(input: &mut String) {
+    io::stdout().flush().unwrap();
+
+    let stdin = io::stdin();
+    stdin.read_line(input).unwrap();
+}
+
+fn parse_command(input: &str) -> Vec<&str> {
+    input.trim().to_lowercase().leak().split(' ').collect()
 }
